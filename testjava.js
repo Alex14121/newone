7ddbf634beb5e7f16e96f5d0dbd093306c387623
alex <oleksandr.@gmail.com>
2020-05-15 16:12:30 +0300

    myfirst

---
+++ testjava.js
@@ -0,0 +1,27 @@
+  'usestrict';
+console.log(4/0);
+console.log('string'*9);
+let something;
+console.log(something);
+let persone = {
+	name: "John",
+	age: 25,
+	isMaried: false
+};
+console.log(persone.name);
+console.log(persone["name"]);
+let arr = ['plum.pmg','orange.jpg','apple.bmp'];
+console.log(arr[1]);
+
+alert("Hello World");
+confirm("are you here?");
+let answer = confirm("are you here?");
+console.log(answer);
+answer = +prompt("Есть ли вам 18 лет?","Да");
+console.log(typeof(answer));
+let incr = 10,
+	decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
---
+++ Новая папка/New.js
@@ -0,0 +1,13 @@
+"use strict";
+//let number = 5;
+//const MyName = 1;
+
+//number = 10;
+//console.log(number);
+
+const answers = [];
+
+answers[0] = prompt('What is your name?','');
+answers[1] = prompt('What is your last name?','');
+answers[2] = prompt('How old are you?','');
+
